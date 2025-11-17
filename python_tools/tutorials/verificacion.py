daa81b74fb638a472d9630ad4bcc3bb19e162a3e
sebastianVP <alex.valdezp22@gmail.com>
2025-11-17 12:20:59 -0500

    update tools

--- python_tools/tutorials/verificacion.py
+++ python_tools/tutorials/verificacion.py
@@ -1,3 +1,12 @@
+"""
+“pkg_resources is deprecated”
+💬 Es solo un WARNING, no afecta el funcionamiento.
+Viene desde el paquete pyrtlsdr, que todavía usa pkg_resources.
+En 2025 esa API desaparecerá, pero por ahora todo funciona normal.
+"""
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
 from rtlsdr import RtlSdr
 
 sdr = RtlSdr()
